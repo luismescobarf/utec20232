858e73f54a6f6c5039b5353d626dfa05a153bbfe
luismescobarf <luismescobarf@gmail.com>
2023-07-11 18:03:48 -0500

    Creación de variables en clase, primeros ejemplos

---
+++ Clase1/ejemploVariables.py
@@ -0,0 +1,24 @@
+a = 2.34
+b = 32
+a = int(a)
+c = a + b
+print(c)
+
+alturaTriangulo = 12 #Camel Case
+AlturaTriangulo = 12 #Pascal Case
+altura_triangulo = 12 #Snake Case
+
+var1 = var2 = var3 = n = 34.8
+
+var1, var2, var3 = 10 , 20 , 30
+print(type(n))
+
+numero1 = 5.2
+numero2 = 2
+resultadoDivEntera = int(numero1 // numero2)
+
+numero3 = 15
+numero4 = 3
+resultadoResiduo = numero3 % numero4 #MOD
+
+
