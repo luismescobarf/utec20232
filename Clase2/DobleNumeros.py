aa57faa1c1960282ded6ab2b589c168fe7000142
luismescobarf <luismescobarf@gmail.com>
2023-07-13 16:54:02 -0500

    Segundo ejmplo conceptualizacion y transcripcion

---
+++ Clase2/DobleNumeros.py
@@ -0,0 +1,28 @@
+"""
+
+- Problema: se necesita un algoritmo
+que reciba dos valores y calcule 
+el doble de la suma de estos.
+
+Pseudocódigo
+1) Guardar los 2 valores en variables
+2) Sumar los dos valores y guardar el resultado
+3) Duplicar el resultado guardado
+4) Mostrar el resultado en pantalla
+
+"""
+
+#Transcripción
+
+#1) Guardar los 2 valores en variables
+valor1 = 2
+valor2 = 4
+
+#2) Sumar los dos valores y guardar el resultado
+suma = (valor1 + valor2)
+
+#3) Duplicar el resultado guardado
+duplicado = suma * 2
+
+#4) Mostrar el resultado en pantalla
+print(f"El resultado es: {duplicado}")
