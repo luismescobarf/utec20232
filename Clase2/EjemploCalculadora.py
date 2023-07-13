2d413b37f3530a0d987c7ddb7e355357e87500fb
luismescobarf <luismescobarf@gmail.com>
2023-07-13 16:44:11 -0500

    Primer ejemplo

---
+++ Clase2/EjemploCalculadora.py
@@ -0,0 +1,26 @@
+"""
+Problema:
+    
+Totalizar la siguiente de lista de productos con una calculadora:
+
+Huevos 50
+Leche 20
+Pan 18
+Manzanas 19
+
+Pasos (Pseudocódigo):
+1) Ans = 50 + 20
+2) Ans = Ans + 18
+3) Ans = Ans + 19
+4) Mostrar Ans en pantalla
+"""
+
+#Transcripción
+Ans = int()
+Ans = 50 + 20
+Ans = Ans + 18
+Ans = Ans + 19
+print(Ans)
+
+
+
