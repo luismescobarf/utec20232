2ccbcceadb79a3e2e50afda6e64606b98f80c32d
luismescobarf <luismescobarf@gmail.com>
2023-07-13 17:07:22 -0500

    Jerarquía de signos

---
+++ Clase2/jerarquiaSignos.py
@@ -0,0 +1,21 @@
+#Jerarquía de Signos
+
+"""
+0) Paréntesis (modificar el orden)
+1) Potencia (derecha a izquierda)
+2) Multiplicación, División, División Entera y Residuo 
+(izquierda a derecha)
+3) Suma y resta (izquierda a derecha)
+"""
+
+#Carga de las variables
+a = 1
+b = 2
+c = 1
+d = 4
+
+#Calcular x
+x = (a+b) / (c-d)
+
+#Mostrarlo en pantalla
+print(x)
