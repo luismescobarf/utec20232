10af12171622c106d8cb8e86997635bf2a99dccf
luismescobarf <luismescobarf@gmail.com>
2023-07-13 17:44:13 -0500

    Uso de funciones implementadas del lenguaje

---
+++ Clase2/UtilizandoFuncionesPython.py
@@ -0,0 +1,16 @@
+"""
+Escribir un programa que reciba
+4 edades y muestra cuál es la mayor
+y cuál es la menor
+"""
+
+Edad1 = 15
+Edad2 = 37
+Edad3 = 44
+Edad4 = 62
+
+MayorEdad = max(Edad1, Edad2, Edad3, Edad4)
+MenorEdad = min(Edad1, Edad2, Edad3, Edad4)
+
+print(f"La mayor edad es {MayorEdad} y la menor edad es {MenorEdad}")
+
