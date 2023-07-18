316cfed40543137fb669a0279bb1625c56887964
luismescobarf <luismescobarf@gmail.com>
2023-07-18 17:22:11 -0500

    Primer ejemplo de aplicación de modularidad

---
+++ Clase3/AreaCirculo.py
@@ -0,0 +1,1 @@
+#Algoritmo#Obtener el radio del círculo#Calcular el área#Presentarlo en pantallaradio = 5area = 3.1416 * (radio**2)print(f"El área del círculo es: {area}")
---
+++ Clase3/AreaCuadrado.py
@@ -0,0 +1,1 @@
+#Algoritmo#Obtener el lado del cuadrado#Calcular el área#Presentarlo en pantallalado = 4area = lado ** 2print(f"El área del cuadrado es: {area}")
