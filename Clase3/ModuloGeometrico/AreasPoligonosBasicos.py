2fd0a3f9e451e8b97ab7e20621d59b37979ba378
luismescobarf <luismescobarf@gmail.com>
2023-07-18 17:59:37 -0500

    Introducción a módulos

---
+++ Clase3/ModuloGeometrico/AreasPoligonosBasicos.py
@@ -0,0 +1,1 @@
+def calcularAreaCudarado(lado):    area = lado ** 2    return areadef calcularAreaCirculo(radio):    area = 3.1416 * (radio**2)    return area
---
+++ Clase3/VolumenesSolidosBasicos.py
@@ -0,0 +1,1 @@
+def calcularVolumenParalelepipedo(largo,ancho,alto):    return largo * ancho * altodef calcularVolumenCilindro(radio,altura):        return 3.1416 * (radio**2) * altura
