a73ff03d15082549d2d1bac1bf34f016cef848ae
luismescobarf <luismescobarf@gmail.com>
2023-08-10 16:27:49 -0500

    Notación diccionarios

---
+++ Clase9/notacionDiccionarios.py
@@ -0,0 +1,29 @@
+#Variables -> Entidad (procesar)
+nombre = "Camila"
+edad = 30
+becada = True
+promedio = 4.8
+
+#Representar Otra Persona
+persona = dict()
+persona['nombre'] = 'Manuel' 
+persona['edad'] = 42
+persona['promedio'] = 4.9
+
+persona2 = {
+        'nombre' : 'Alejandra',
+        'becada' : True,
+        'promedio' : 4.8
+    }
+
+def procesarPersona(nombre,
+                    edad,
+                    becada,
+                    promedio):
+    print("Paso1")
+    print("Paso2")
+    return 1
+
+def procesarPersona2(persona):
+    pass
+
