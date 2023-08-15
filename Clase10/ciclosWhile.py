b0dedba5cc127863ab7267397f0e03175276e312
luismescobarf <luismescobarf@gmail.com>
2023-08-15 16:50:20 -0500

    Ejemplo básico de while

---
+++ Clase10/ciclosWhile.py
@@ -0,0 +1,29 @@
+#1) Algoritmo  para imprimir los números 
+#del 1 al 10
+
+#Solución especificando las repeticiones
+# print("1")
+# print("2")
+# print(".")
+# print(".")
+# print(".")
+# print("10")
+
+#Generalización
+
+#Estados previos al inicio de las repeticiones
+n = 1#Variable(s) involucradas en el cómputo
+i = 0#Contador (controlar las repeticiones)
+
+while i < 10:
+    #Generalización
+    print(n)
+    #Actualizaciones (valor de i, n)
+    i = i + 1
+    n = n + 1
+    print('---')
+    
+print("Proceso posterior al ciclo mientras")
+
+
+
