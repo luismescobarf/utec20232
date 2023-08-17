d236834b6df8cd016e3d862b69e4b6d3fcade563
luismescobarf <luismescobarf@gmail.com>
2023-08-17 17:19:22 -0500

    Camino alterno de los ciclos

---
+++ Clase11/caminoAlternoCiclo.py
@@ -0,0 +1,17 @@
+#Librería
+import random
+
+i = random.randint(7,20)
+
+while i < 10:
+    print(f"Cómputo Simulado 1 - {i}")
+    print(f"Cómputo Simulado 2 - {i}")
+    i += 1
+else:
+    print("Condición ya no se satisface...")
+
+print("...")
+print("Continúa el proceso!!!")
+print("Continúa el proceso!!!")
+
+    
