fc0ddadcfbd70a7cebfbad2ab6c556c18f4c1720
luismescobarf <luismescobarf@gmail.com>
2023-09-05 16:19:28 -0500

    Agregamos atributo codigo para identificar los servicios no por su posicion en el contenedor del set

--- Clase16/csp.py
+++ Clase16/csp.py
@@ -8,6 +8,7 @@ coleccionServicios = []
 
 # #Contenedor ejemplo de servicio
 # servicio = {
+#     'codigo': 10
 #     't0':400,
 #     'tf':445,
 #     'duracion':45
@@ -18,13 +19,14 @@ coleccionServicios = []
 numeroServicios = 20
 cuatroAM = 240 #Minutos
 diezPM = 1320 #Minutos
-for _ in range(numeroServicios):
+for i in range(numeroServicios):
     t0 = random.randint(cuatroAM,diezPM)
     tf = int()
     while True:
         tf = random.randint(cuatroAM,diezPM)
         if tf > t0 and tf-t0 <= 180 and tf-t0 > 15:
             servicio = {
+                'codigo': i,
                 't0':t0,
                 'tf':tf,
                 'duracion':tf-t0
@@ -36,27 +38,27 @@ pp.pprint(coleccionServicios)
 #Caso de estudio de asignación
 #Caso 1
 coleccionServicios = [
-    {'duracion': 27, 't0': 631, 'tf': 658},
-    {'duracion': 121, 't0': 1170, 'tf': 1291},
-    {'duracion': 98, 't0': 932, 'tf': 1030},
-    {'duracion': 110, 't0': 801, 'tf': 911},
-    {'duracion': 107, 't0': 780, 'tf': 887},
-    {'duracion': 36, 't0': 828, 'tf': 864},
-    {'duracion': 70, 't0': 1021, 'tf': 1091},
-    {'duracion': 143, 't0': 885, 'tf': 1028},
-    {'duracion': 176, 't0': 245, 'tf': 421},
-    {'duracion': 168, 't0': 341, 'tf': 509},
-    {'duracion': 72, 't0': 470, 'tf': 542},
-    {'duracion': 16, 't0': 712, 'tf': 728},
-    {'duracion': 171, 't0': 430, 'tf': 601},
-    {'duracion': 94, 't0': 1196, 'tf': 1290},
-    {'duracion': 68, 't0': 787, 'tf': 855},
-    {'duracion': 175, 't0': 345, 'tf': 520},
-    {'duracion': 116, 't0': 700, 'tf': 816},
-    {'duracion': 136, 't0': 661, 'tf': 797},
-    {'duracion': 32, 't0': 633, 'tf': 665},
-    {'duracion': 126, 't0': 1049, 'tf': 1175}
-]
+    {'codigo': 0, 'duracion': 66, 't0': 1100, 'tf': 1166},
+    {'codigo': 1, 'duracion': 103, 't0': 915, 'tf': 1018},
+    {'codigo': 2, 'duracion': 68, 't0': 1024, 'tf': 1092},
+    {'codigo': 3, 'duracion': 97, 't0': 749, 'tf': 846},
+    {'codigo': 4, 'duracion': 82, 't0': 1055, 'tf': 1137},
+    {'codigo': 5, 'duracion': 133, 't0': 620, 'tf': 753},
+    {'codigo': 6, 'duracion': 153, 't0': 879, 'tf': 1032},
+    {'codigo': 7, 'duracion': 160, 't0': 538, 'tf': 698},
+    {'codigo': 8, 'duracion': 144, 't0': 299, 'tf': 443},
+    {'codigo': 9, 'duracion': 90, 't0': 714, 'tf': 804},
+    {'codigo': 10, 'duracion': 45, 't0': 1005, 'tf': 1050},
+    {'codigo': 11, 'duracion': 73, 't0': 514, 'tf': 587},
+    {'codigo': 12, 'duracion': 81, 't0': 914, 'tf': 995},
+    {'codigo': 13, 'duracion': 83, 't0': 341, 'tf': 424},
+    {'codigo': 14, 'duracion': 57, 't0': 335, 'tf': 392},
+    {'codigo': 15, 'duracion': 179, 't0': 657, 'tf': 836},
+    {'codigo': 16, 'duracion': 49, 't0': 432, 'tf': 481},
+    {'codigo': 17, 'duracion': 55, 't0': 1230, 'tf': 1285},
+    {'codigo': 18, 'duracion': 21, 't0': 636, 'tf': 657},
+    {'codigo': 19, 'duracion': 66, 't0': 1122, 'tf': 1188}
+ ]
 
 #Inicializar cuadro de turnos -> Lista (también podría ser un diccionario)
 cuadroTurnos = list()
