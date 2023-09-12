4c4d38d242020a5e7aa4fd837b8ee926fab51636
luismescobarf <luismescobarf@gmail.com>
2023-09-12 16:15:36 -0500

    Archivos iniciales Sesión 18

---
+++ Clase18/archivosJSON_1.py
@@ -0,0 +1,139 @@
+#Librerías
+import random
+import pprint as pp
+import json
+
+#Definición de funciones
+def generarCasoCSP():
+    coleccionServicios = []
+    
+    #Generación aleatoria de servicios
+    numeroServicios = 20
+    cuatroAM = 240 #Minutos
+    diezPM = 1320 #Minutos
+    for i in range(numeroServicios):
+        t0 = random.randint(cuatroAM,diezPM-16)
+        while True:
+            tf = random.randint(t0,diezPM)
+            if tf > t0 and tf-t0 <= 180 and tf-t0 > 15:
+                servicio = {
+                    'codigo': i,
+                    't0':t0,
+                    'tf':tf,
+                    'duracion':tf-t0
+                }
+                coleccionServicios.append(servicio)
+                break    
+    return coleccionServicios
+    
+
+
+#Caso de estudio de asignación
+#Caso 1
+coleccionServicios = [
+    {'codigo': 0, 'duracion': 66, 't0': 1100, 'tf': 1166},
+    {'codigo': 1, 'duracion': 103, 't0': 915, 'tf': 1018},
+    {'codigo': 2, 'duracion': 68, 't0': 1024, 'tf': 1092},
+    {'codigo': 3, 'duracion': 97, 't0': 749, 'tf': 846},
+    {'codigo': 4, 'duracion': 82, 't0': 1055, 'tf': 1137},
+    {'codigo': 5, 'duracion': 133, 't0': 620, 'tf': 753},
+    {'codigo': 6, 'duracion': 153, 't0': 879, 'tf': 1032},
+    {'codigo': 7, 'duracion': 160, 't0': 538, 'tf': 698},
+    {'codigo': 8, 'duracion': 144, 't0': 299, 'tf': 443},
+    {'codigo': 9, 'duracion': 90, 't0': 714, 'tf': 804},
+    {'codigo': 10, 'duracion': 45, 't0': 1005, 'tf': 1050},
+    {'codigo': 11, 'duracion': 73, 't0': 514, 'tf': 587},
+    {'codigo': 12, 'duracion': 81, 't0': 914, 'tf': 995},
+    {'codigo': 13, 'duracion': 83, 't0': 341, 'tf': 424},
+    {'codigo': 14, 'duracion': 57, 't0': 335, 'tf': 392},
+    {'codigo': 15, 'duracion': 179, 't0': 657, 'tf': 836},
+    {'codigo': 16, 'duracion': 49, 't0': 432, 'tf': 481},
+    {'codigo': 17, 'duracion': 55, 't0': 1230, 'tf': 1285},
+    {'codigo': 18, 'duracion': 21, 't0': 636, 'tf': 657},
+    {'codigo': 19, 'duracion': 66, 't0': 1122, 'tf': 1188,
+     'personas': ['Aldo','Maria','Ana']}
+ ]
+
+#Generar un archivo con la información de la colección
+rutaArchivo = "json/caso1Servicios.json"
+
+
+
+# #Mientras el archivo esté abierto
+# with open(rutaArchivo,"w") as f:
+    
+#     #Descargar información en el archivo
+#     for i,servicio in enumerate(coleccionServicios):
+#         linea = str()
+#         linea += str(servicio['codigo']) + " "
+#         linea += str(servicio['duracion']) + " "
+#         linea += str(servicio['t0']) + " "
+#         linea += str(servicio['tf']) + "\n"
+#         f.write(linea)
+
+# print("Instrucciones posteriores al cierre del archivo")
+
+# #Lectura del archivo generado
+# coleccionDesdeArchivo = list()
+# rutaRecurso = "recursos/loteServicios.txt"
+# # rutaRecurso = "loteServicios.txt"
+
+
+#Generar archivo json del contenedor o ED de nuestros servicios
+# try:
+#     with open(rutaArchivo,"w") as f:
+#         json.dump(coleccionServicios,f)
+# except FileNotFoundError:
+#     print("Error en la ruta para almacenar el archivo!!!")
+    
+
+
+# try: 
+    
+#     with open(rutaRecurso,"r") as f:
+#         for linea in f.readlines():
+#             linea = linea.strip()
+#             registro = linea.split(" ")
+#             servicio = {
+#                     'codigo': int(registro[0]), 
+#                     'duracion': int(registro[1]),  
+#                     't0': int(registro[2]), 
+#                     'tf': int(registro[3]) 
+#             }
+#             coleccionDesdeArchivo.append(servicio)
+            
+# except FileNotFoundError:
+#     print("Archivo no encontrado!!! Generando set aleatorio")
+#     coleccionDesdeArchivo = generarCasoCSP()
+#     print("Fin de caso alterno")
+
+
+# print("Contenido cargado desde el archivo:")
+# pp.pprint(coleccionDesdeArchivo)
+
+#Cargar un archivo JSON en memoria o en ED -> Procesamiento
+coleccionDesdeArchivo = None
+try:
+    with open(rutaArchivo,"r") as f:
+        coleccionDesdeArchivo=json.load(f)
+except FileNotFoundError:
+    print("Error en la ruta para cargar el json!!!")
+
+print(f"Tipo de la ED -> {type(coleccionDesdeArchivo)}")
+pp.pprint(coleccionDesdeArchivo)
+ 
+        
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Clase18/tspV2.py
@@ -0,0 +1,193 @@
+#Librerías
+import pprint as pp
+import random 
+
+
+
+
+
+
+    
+
+#Distancia Euclidiana
+def distanciaEuclidiana(P1,P2):
+    return  int(( (P1[0] - P2[0])**2 + (P1[1] - P2[1])**2 )**(1/2))
+
+#Traducción de nombre a índice de la estructura de datos de nodos planteada
+def traducirNombreIndice(nombrePunto,nodos):    
+    for i,nodo in enumerate(nodos):
+        if nombrePunto == nodo['Nombre']:
+            return i
+        
+#Función para calcular distancia total de una ruta
+def calcularDistancia(ruta,nodos,conexiones):
+    distanciaTotal = 0
+    strRuta = str()
+    for i in range(len(ruta)-1): 
+        nombrePartida = nodos[ ruta[i] ]['Nombre']
+        nombreLlegada = nodos[ ruta[i+1] ]['Nombre']
+        nombreConexion = nombrePartida + '-' + nombreLlegada
+        
+        # #Salida de diagnóstico
+        # print("->",nombreConexion)
+        
+        #Acumular la conexión con las etiquetas originales
+        strRuta += " ->"+nombreConexion
+        
+        distanciaTotal += conexiones[nombreConexion][2]
+    
+    return distanciaTotal,strRuta    
+
+#Algoritmo para encontrar el recorrido para el TSP
+#Vecino más cercano NN Nearest Neigborgh
+def heuristicaNN_UnicoArranque(nodos,conexiones):
+
+    #Esquema de una solución (incumbente)
+    incumbente = {
+        'nodoPartida': str(),
+        'funcionObjetivo' : 99999,
+        'ruta': [],
+        'strRuta': str()
+    }   
+    
+    #Punto de arranque fijo
+    ii = 0
+    
+    #Establecer estructuras de control de los nodos que han sido cubiertos y los que no
+    nodosCubiertos = set()
+    nodosPendientes = set()
+    for i in range(len(nodos)):
+        nodosPendientes.add(i)
+        
+    #Inicialización -> Nodo ii
+    nodoInicial = ii
+    ruta = [nodoInicial]
+    #Cubrir nodo inicial
+    nodosCubiertos.add(nodoInicial)
+    #Quitar el nodo inicial de los pendientes
+    nodosPendientes.remove(nodoInicial)
+    
+    #Ciclo general del algoritmo
+    while nodosPendientes != set():
+    #while len(nodosPendientes) > 0:
+        
+        # #Salida de diagnóstico
+        # print("-->Estado de la ruta")
+        # pp.pprint(ruta)        
+        
+        #Último nodo cubierto -> Nodo actual
+        nodoActual = ruta[-1]
+        
+        #Incumbente de posibles salidas
+        incumbenteSalidas = (-1,-1,float('inf'))       
+        
+        #Calcular cada salida y revisar decidir si se realiza o no actualización de la incumbente
+        for nodoPendiente in nodosPendientes:
+            costoSalida = distanciaEuclidiana(
+                (nodos[nodoActual]['x'],nodos[nodoActual]['y']),
+                (nodos[nodoPendiente]['x'],nodos[nodoPendiente]['y']),
+            )
+            if costoSalida < incumbenteSalidas[-1]:
+                incumbenteSalidas = (nodoActual,nodoPendiente,costoSalida)         
+        
+        
+        # #Salida de diagnóstico
+        # print("Salida ganadora: ")        
+        # print(incumbenteSalidas)
+        # input()
+        
+        
+        #Traducir la mejor salida obtenida        
+        indiceNodoSalida = incumbenteSalidas[1]
+        ruta.append(indiceNodoSalida)
+        nodosCubiertos.add(indiceNodoSalida)
+        nodosPendientes.remove(indiceNodoSalida)
+    
+    #Completar la ruta
+    ruta.append(nodoInicial)
+    distanciaTotal, strRuta = calcularDistancia(ruta,nodos,conexiones)
+    
+    #Acumular la solución
+    solucion = {
+        'nodoPartida': nodos[ ruta[0] ]['Nombre'],
+        'funcionObjetivo' : distanciaTotal,
+        'ruta': ruta,
+        'strRuta': strRuta
+    }        
+    
+    #Retornar la solución en la estructura de datos preestablecida
+    return solucion
+
+
+   
+    
+#Sección principal
+##################
+
+#Represntar red
+nodos = [] #Permite asociar un id a cada uno de los elementos
+
+#Nodo a nodo adición en esa lista
+nodos.append({'Nombre':'A','x':0,'y':0})
+nodos.append({'Nombre':'B','x':10,'y':10})
+nodos.append({'Nombre':'C','x':-10,'y':-3})
+nodos.append({'Nombre':'D','x':-5,'y':8})
+nodos.append({'Nombre':'E','x':11,'y':-2})
+nodos.append({'Nombre':'Z','x':13,'y':5})
+
+# #Construir ED
+# conexiones = [
+#  ('A','B', distanciaAB),
+#  ('A','D', distanciaAD),
+#  .
+#  .
+#  .
+#  ('E','D', distanciaED),
+ 
+#  ]
+
+# #Estructura de datos con el cálculo de las distancias
+# #Esta información se calcula y no quiero que se modifique
+# conexiones = []
+# for i in range(len(nodos)):
+#     for j in range(len(nodos)):
+#         #Nodos diferentes (prevenir conexiones a un mismo nodo)
+#         if i != j:
+#             conexiones.append(  #Inicio de la tupla que será coleccionada en las conexiones
+#                                 (nodos[i]['Nombre'],
+#                                  nodos[j]['Nombre'],
+#                                  distanciaEuclidiana(
+#                                     (nodos[i]['x'],nodos[i]['y']),
+#                                     (nodos[j]['x'],nodos[j]['y'])
+#                                  )
+#                                 )#Final de la tupla por cada conexión
+#                               )#Final del append que acumula en el listado de conexiones
+            
+#Estructura de datos con el cálculo de las distancias (diccionario)
+#Esta información se calcula y no quiero que se modifique
+conexiones = dict()
+for i in range(len(nodos)):
+    for j in range(len(nodos)):
+        #Nodos diferentes (prevenir conexiones a un mismo nodo)
+        if i != j:
+            conexiones[ nodos[i]['Nombre'] + '-' + nodos[j]['Nombre'] ] = (
+                                    nodos[i]['Nombre'],
+                                    nodos[j]['Nombre'],
+                                    distanciaEuclidiana(
+                                        (nodos[i]['x'],nodos[i]['y']),
+                                        (nodos[j]['x'],nodos[j]['y'])
+                                    )
+                                )#Final de la tupla por cada conexión
+
+
+#Comparación de heurísticas
+#--------------------------
+
+#Llamado a heurística y salidas en pantalla de indicadores
+print("---->Vecino Más Cercano Único Arranque")
+pp.pprint(heuristicaNN_UnicoArranque(nodos, conexiones)) 
+
+
+#Estudio comparativo con otros arranques...
+
+#Estudio con otros constructivos
